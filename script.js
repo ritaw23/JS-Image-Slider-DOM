8f2ca15c225fd9196d571dada6a0885b809912df
ritzzw23 <24001085-ritzzw23@users.noreply.replit.com>
2024-01-09 19:55:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// select arrows
+const prevArrow = document.querySelector('.prev');
+const nextArrow = document.querySelector('.next');
+
+// save the number of images inside the container
+
+const carouselContainer = document.querySelector('.carousel').children;
+
+// the number of children and save in a new variable
+let totalSlides = carouselContainer.length;
+let index = 0;
+
+// add event listener to the arrows
+
+prevArrow.addEventListener('click', () => {
+  moveSlides("prev");
+});
+
+nextArrow.addEventListener('click', () => {
+  moveSlides("next");
+});
+
+const moveSlides = (direction) => {
+
+  if(direction === 'next') {
+    index++;
+    if(index === totalSlides) {
+      index = 0;
+    }
+  } else {
+    if(index === 0) {
+      index = totalSlides - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for(let i = 0; i < totalSlides; i++) {
+    carouselContainer[i].classList.remove('main');
+  }
+
+carouselContainer[index].classList.add('main');
+  console.log(index);
+
+};
+
+setInterval(() => {
+  moveSlides("next");
+}, 2000);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
